b9e7890d25f53e89f18131d2bb9a9eddb5d8ee79
RedRingRico <teh_programerer@interactivelaboratories.com>
2014-10-20 15:37:11 +0100

    Initial commit
    |INITIAL COMMIT|
    * Added Git ignore rules
    * Added a basic README
    * Added a basic Makefile for creating a SEGA Saturn program
    * Added a GNU Linker script for the SEGA Saturn
    * Added a program which does nothing of import (loops indefinitely)

---
+++ Source/Source/Main.c
@@ -0,0 +1,30 @@
+#define SYSTEM_WORK	0x060FFC00
+#define SYSTEM_SIZE	( 0x06100000 - 0x060FFC00 )
+
+extern unsigned int _BSSBegin, _BSSEnd;
+
+void main( void )
+{
+	unsigned char *pDestination;
+	unsigned int Index;
+
+	/* Zero the .bss */
+	for( pDestination = ( unsigned char * )&_BSSBegin;
+		pDestination < ( unsigned char * )&_BSSEnd; ++pDestination )
+	{
+		*pDestination = 0;
+	}
+
+	/* This is for clearing the SGL system variables */
+	for( pDestination = ( unsigned char * )SYSTEM_WORK, Index = 0;
+		Index < SYSTEM_SIZE; ++Index )
+	{
+		*pDestination = 0;
+	}
+
+	while( 1 )
+	{
+		/* Just keep going */
+	}
+}
+
