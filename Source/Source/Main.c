3908747dc70f93a14109221f0c5534a3f442d618
Rico Tyrell <teh_programerer@interactivelaboratories.com>
2015-07-19 04:04:55 +0100

    Added an output function when compiling for the USB Development Cartridge
    |BUILD CONFIGURATION|
    [Makefile]
    * DEVCART option added to allow for certain USB Development Cartridge functions
      to work
    
    |SOURCE|
    [Main]
    * Added a StringOut function to print to the USB Development Cartridge transfer
      utility console

--- Source/Source/Main.c
+++ Source/Source/Main.c
@@ -1,7 +1,14 @@
+#include <stdint.h>
 #define SYSTEM_WORK	0x060FFC00
 #define SYSTEM_SIZE	( 0x06100000 - 0x060FFC00 )
 
 extern unsigned int _BSSBegin, _BSSEnd;
+#define USB_FIFO	( *( volatile uint8_t * )( 0x22100001 ) )
+#define USB_FLAGS	( *( volatile uint8_t * )( 0x22200001 ) )
+#define USB_TXE		( 1 << 1 )
+#define WAIT_FOR_WRITE_FIFO( ) do { while( ( USB_FLAGS & USB_TXE ) ); }while( 0 )
+
+void StringOut( const char *p_pString );
 
 void main( void )
 {
@@ -22,9 +29,26 @@ void main( void )
 		*pDestination = 0;
 	}
 
+	StringOut( "SEGA Saturn SDK test string\n" );
+
 	while( 1 )
 	{
 		/* Just keep going */
 	}
+}
+
+void StringOut( const char *p_pString )
+{
+#if defined BUILD_USBDEVCART
+	char Print = ( *p_pString );
+
+	while( Print != '\0' )
+	{
+		WAIT_FOR_WRITE_FIFO( );
+		USB_FIFO = Print;
+
+		Print = ( *++p_pString );
+	}
+#endif
 }
 
